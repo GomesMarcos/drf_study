66e8afbb27968b1b60dcd66e98ff11877d6027af
gomesmarcos <gomes.marcosjf@gmail.com>
2020-07-18 09:06:29 -0300

    versionando API  parte 2: implementando decorator @action para acessar as avaliações de cursos específicos.

--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 
+""" V2 """
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -56,6 +59,12 @@ API V2
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
 
 class AvaliacaoViewSet(viewsets.ModelViewSet):
     queryset = Avaliacao.objects.all()
