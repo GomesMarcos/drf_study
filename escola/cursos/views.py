6b119e2cc1b964e3ad811f2bd3b3026a9e7fbdd2
gomesmarcos <gomes.marcosjf@gmail.com>
2020-07-18 08:49:41 -0300

    sobrescrita do DRF CRUD com ModelViewSet, versionando API para V2 com SimpleRouter

--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,18 +1,24 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 
+from rest_framework import viewsets
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
+"""
+API V1
+"""
+
 class CursosAPIView(generics.ListCreateAPIView):
-    """ 
+    """
     API DE CURSOS
     """
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class AvaliacoesAPIView(generics.ListCreateAPIView):
-    """ 
+    """
     API DE AVALIAÇÕES
     """
     queryset = Avaliacao.objects.all()
@@ -24,14 +30,14 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
         return self.queryset.all()
 
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """ 
+    """
     API SINGLE DE CURSOS
     """
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """ 
+    """
     API SINGLE DE AVALIAÇÕES
     """
     queryset = Avaliacao.objects.all()
@@ -41,3 +47,16 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
         if self.kwargs.get('cursp_pk'):
             return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
         return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
+
+"""
+API V2
+"""
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
